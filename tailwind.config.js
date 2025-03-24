e2c8625d41efc369409e54fd6ba25001d2fa07e3
gabkoi21 <12345678+username@users.noreply.github.com>
2025-03-24 00:39:03 +0000

    Add the final editing for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-import flowbite from "flowbite/plugin";
+import flowbite from 'flowbite/plugin';
 
 module.exports = {
   content: [
-    "./src/index.html",
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./src/components/**/*.{html,js,jsx,ts,tsx}",
-    "./src/pages/**/*.{html,js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",
+    './src/index.html',
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './src/components/**/*.{html,js,jsx,ts,tsx}',
+    './src/pages/**/*.{html,js,jsx,ts,tsx}',
+    './public/pages/**/*.{html,js,jsx,ts,tsx}',
+    './public/pages/**/*.{html,js,jsx,ts,tsx}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
+        roboto: ['Roboto', 'sans-serif'],
       },
       colors: {
-        "primary-color": "#09122c",
-        "secondary-color": "#f9fafb",
+        'primary-color': '#09122c',
+        'secondary-color': '#f9fafb',
       },
     },
   },
   plugins: [flowbite],
-  darkMode: "class",
+  darkMode: 'class',
 };
